90a637317b221f07204a2cfcec3d0e55a7aecaa8
jsherzan <77797866+jsherzan@users.noreply.github.com>
2021-02-01 17:34:51 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ch02FutueValueSherzan.Models;
+
+namespace Ch02FutueValueSherzan.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
